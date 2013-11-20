b7a2d71f4beb81848f3869187b6f7c6bb4bedc05
rubinoske <RubenVW@178-119-188-206.access.telenet.be>
2013-11-20 16:23:14 +0100

    Initial Commit

---
+++ GAP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GAP
+//
+//  Created by Ruben Van Wassenhove on 20/11/13.
+//  Copyright Devine 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
