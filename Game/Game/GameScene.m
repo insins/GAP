65e7345a7b21f7c28d70a7fda3b6fe7576ff2b71
rubinoske <rubenvanwassenhove@gmail.com>
2013-12-09 12:34:47 +0100

    Merge branch 'test-microfoon'

--- Game/Game/GameScene.m
+++ Game/Game/GameScene.m
@@ -35,7 +35,10 @@
         self.difficulty = 1;
         self.countUp = 0;
         
-        //motionmanager instellen
+        // --------------------------------------
+        // motionmanager instellen
+        // --------------------------------------
+        
         self.motionManager = [[CMMotionManager alloc] init];
         self.motionManager.gyroUpdateInterval = 1/self.frequency;
         
@@ -54,15 +57,35 @@
         [self addChild:self.player];
         [self addChild:self.world];
         
+        //op yes zetten om te testen (zou eigenlijk op andere plaats moeten staan)
+        Player *pl = (Player*)self.player;
+        pl.canPlayerBlow = YES;
+        
+        //luister naar event die wordt uitgestuurd als speler blaast en hij kan blazen
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(addLive:) name:@"blow" object:nil];
+        
     }
     return self;
 }
 
+// --------------------------------------
+// Toon de gameover scene
+// --------------------------------------
+
 -(void)gameOver{
+    
     SKTransition *reveal = [SKTransition pushWithDirection:SKTransitionDirectionDown duration:0.5];
     SKScene *gfScene = [[GameFinishedScene alloc] initWithSize:self.frame.size collected:self.collected score:self.score];
     [self.view presentScene:gfScene transition:reveal];
 }
+
+// --------------------------------------
+// Deze method wordt per frame uitgevoerd
+// --------------------------------------
+
+//stel hier de positie van de player in
+//beweeg world (waar enemies en items op worden geplaatst)
+//voeg enemies en items toe aan world adhv interval
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
@@ -109,11 +132,38 @@
                 self.level++;
                 self.difficulty = (self.level - 1) * 128;
                 self.countUp = 0;
+                
+                //hier zou je bijvoorbeeld kunnen blazen
+                Player *pl = (Player*)self.player;
+                pl.canPlayerBlow = YES;
+                
             }
         }
         
     }
 }
+
+// --------------------------------------
+// Deze functie wordt uitgevoerd als speler
+// blaast en er leven mag verhoogd worden
+// --------------------------------------
+
+-(void) addLive:(id)sender{
+    self.lives++;
+}
+
+// -------------------------------------
+// Hitdetection
+// -------------------------------------
+
+// er zijn 5 categorieeen:
+// (volgens strength)
+
+// projectile
+// enemie
+// player
+// powerup
+// item
 
 -(void)didBeginContact:(SKPhysicsContact *)contact{
     
@@ -162,18 +212,44 @@
     self.physicsWorld.contactDelegate = self;
 }
 
+// -------------------------------------
+// Levens logica
+// -------------------------------------
+
+// Je begint met 3 levens ( je kan dan 4 keer in totaal geraakt worden)
+//zolang lives > 0, is het geen game over
+
 - (int)lives{
     return _lives;
 }
 
 - (void)setLives:(int)lives{
-    if (lives > 0) {
+    
+    Player *pl = (Player*)self.player;
+    
+    if (lives < 4) {
         _lives = lives;
-        [(Player*)self.player scaleBell:lives];
-    }else{
-        [self gameOver];
+        
+        //re-scale bell adhv aantal leventjes
+        [pl scaleBell:lives];
+        
+        if(lives < 1){
+            [pl stopRecording];
+            [self gameOver];
+        }
+        
+    }else if(lives >= 3){
+        // je kan niet meer blazen want je hebt max aantal levens
+        pl.canPlayerBlow = NO;
     }
 }
+
+// -------------------------------------
+// Levels
+// -------------------------------------
+
+//er zijn 3 levels, als je hoger raakt dan ga je naar volgend level
+//dat wordt bepaald in de update method ^^
 
 - (int)level{
     return _level;
--- Game/Game/Player.m
+++ Game/Game/Player.m
@@ -13,6 +13,7 @@
 
 @synthesize bell = _bell;
 @synthesize fish = _fish;
+@synthesize canPlayerBlow = _canPlayerBlow;
 
 -(id) init{
     
@@ -27,113 +28,76 @@
         self.bell = [SKSpriteNode spriteNodeWithImageNamed:path];
         [self scaleBell:3];
         
-        // -------------------------------------
-        // Levens logica
-        // -------------------------------------
-        
-        // Je begint met 3 levens ( je kan dan 4 keer in totaal geraakt worden)
-        // 3 - 2 - 1 - 0. Wanneer je leven op "-1" komt is het gameover.
-        _aantalLevens = 3;
-        
-        
-        // -------------------------------------
-        // Blazen
-        // -------------------------------------
-        [self checkLevens];
-        [self checkBlazen];
-        
-        // Can player blow staat default op false.
-        _canPlayerBlow = false;
-        
         // Wanneer je het op true zet (bvb hij komt op een tijdstip dat hij de bel groter kan maken en een leven bijmaken)
 
         [self addChild:self.bell];
         [self addChild:self.fish];
+        
+        [self initMicrofoon];
     
     }
     return self;
 }
+
 // -------------------------------------
 // Blazen goed zetten
 // -------------------------------------
--(void)checkBlazen{
-    
-    // Als hij kan blazen dan zet je alles goed
-    if (_canPlayerBlow){
-        NSLog(@"je kan blazen");
+
+//roep deze functie op vanuit GameScene op het moment dat speler mag blazen
+
+-(void)initMicrofoon{
         
         // Hiermee zet je alles goed voor audio input
         // Daarna ga je naar de functie die kijkt of er effectief geblazen wordt.
-        NSURL *url = [NSURL fileURLWithPath:@"/dev/null"];
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setActive:YES error:nil];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryRecord error:nil];
+    
+    NSURL *url = [NSURL fileURLWithPath:@"/dev/null"];;
+    
+    NSDictionary *settings = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithFloat: 44100.0], AVSampleRateKey, [NSNumber numberWithInt: kAudioFormatAppleLossless], AVFormatIDKey, [NSNumber numberWithInt: 1], AVNumberOfChannelsKey, [NSNumber numberWithInt: AVAudioQualityMax], AVEncoderAudioQualityKey, nil];
         
-        NSDictionary *settings = [NSDictionary dictionaryWithObjectsAndKeys:
-                                  [NSNumber numberWithFloat: 44100.0],                 AVSampleRateKey,
-                                  [NSNumber numberWithInt: kAudioFormatAppleLossless], AVFormatIDKey,
-                                  [NSNumber numberWithInt: 1],                         AVNumberOfChannelsKey,
-                                  [NSNumber numberWithInt: AVAudioQualityMax],         AVEncoderAudioQualityKey,
-                                  nil];
+    NSError *error;
         
-        NSError *error;
+    self.recorder = [[AVAudioRecorder alloc] initWithURL:url settings:settings error:&error];
         
-        _recorder = [[AVAudioRecorder alloc] initWithURL:url settings:settings error:&error];
-        
-        if (_recorder) {
-            [_recorder prepareToRecord];
-            _recorder.meteringEnabled = YES;
-            [_recorder record];
-            _levelTimer = [NSTimer scheduledTimerWithTimeInterval: 0.03 target: self selector: @selector(levelTimerCallback:) userInfo: nil repeats: YES];
-        } else
-            NSLog([error description]);
-    }
-    // Kan hij niet blazen dan gaan we gewoon tracen dat hij niet hoeft te blazen
-    else{
-        NSLog(@"Speler moet niet blazen");
+    if ([self.recorder prepareToRecord]) {
+        self.recorder.meteringEnabled = YES;
+            
+        if ([self.recorder record]) {
+            NSLog(@"record");
+            self.timer = [NSTimer scheduledTimerWithTimeInterval: 0.03 target: self selector: @selector(levelTimerCallback:) userInfo: nil repeats: YES];
+        }
+            
+    } else {
+        NSLog(@"%@", [error description]);
     }
 
 }
 
-
+-(void)stopRecording{
+    [self.timer invalidate];
+    self.timer = nil;
+}
 
 // -------------------------------------
 // Functie voor bij het blazen
 // -------------------------------------
 
 - (void)levelTimerCallback:(NSTimer *)timer {
-	[_recorder updateMeters];
+	[self.recorder updateMeters];
     
 	const double ALPHA = 0.05;
-	double peakPowerForChannel = pow(10, (0.05 * [_recorder peakPowerForChannel:0]));
-	_lowPassResults = ALPHA * peakPowerForChannel + (1.0 - ALPHA) * _lowPassResults;
-	
-    // Er wordt niet geblazen
-	if (0.70 < _lowPassResults < 0.95){
-		NSLog(@"no blowing");
-    }
-    // Er wordt wel geblazen
-    else{
-        NSLog(@"Blowing");
-    }
-}
-
-// -------------------------------------
-// Functie om levens te checken
-// -------------------------------------
--(void)checkLevens{
+	double peakPowerForChannel = pow(10, (0.05 * [self.recorder peakPowerForChannel:0]));
+	self.lowPassResults = ALPHA * peakPowerForChannel + (1.0 - ALPHA) * self.lowPassResults;
     
-    if(_aantalLevens < 0){
-        NSLog(@"Game over");
+    // Er wordt geblazen
+	if (self.lowPassResults > 0.70){
+        //event uitsturen om leven te verhogen
+        if (self.canPlayerBlow){
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"blow" object:self];
+        }
     }
-    else if(_aantalLevens == 3){
-        NSLog(@"Er moet niet geblazen worden want full HP");
-    }
-    else{
-        NSLog(@"Er moet geblazen worden want je hebt geen full HP");
-        // Hier zet je dan canPlayerBlow op true zodat je kan blazen
-        _canPlayerBlow = true;
-        
-    }
-    
-    
 }
 
 // -------------------------------------
@@ -147,6 +111,16 @@
     [self resetPhysicsBody];
 }
 
+// -------------------------------------
+// Functie om power te geven aan player
+// -------------------------------------
+
+-(void)addPower:(NSString*)type{
+    //power geven aan player
+    //= visuals aanpassen en meer player intput creeren
+    NSLog(@"%@", type);
+}
+
 -(void)resetPhysicsBody{
     //physicsbody instellen adh grootte van bel
     
@@ -158,13 +132,5 @@
     self.physicsBody.collisionBitMask = 0;
     self.physicsBody.usesPreciseCollisionDetection = YES;
 }
-
--(void)addPower:(NSString*)type{
-    //power geven aan player
-    //= visuals aanpassen en meer player intput creeren
-    NSLog(@"%@", type);
-}
-
-
 
 @end
